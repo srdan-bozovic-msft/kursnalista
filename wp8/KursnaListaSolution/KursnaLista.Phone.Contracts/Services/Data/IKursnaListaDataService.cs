f6283e9ccc27556dfa5238d79b15b624e5c135ae
zmajcek <srdjan.bozovic@live.com>
2013-11-20 17:11:42 +0100

    update repository

---
+++ wp8/KursnaListaSolution/KursnaLista.Phone.Contracts/Services/Data/IKursnaListaDataService.cs
@@ -0,0 +1,16 @@
+﻿using KursnaLista.Phone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KursnaLista.Phone.Contracts.Services.Data
+{
+    public interface IKursnaListaDataService
+    {
+        Task<KursnaListaZaDan> GetNajnovijaKursnaListaAsync(CancellationToken cancellationToken);
+        Task UpdateCache();
+    }
+}
