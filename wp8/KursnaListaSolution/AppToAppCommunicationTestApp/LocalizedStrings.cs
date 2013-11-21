0eb69bede99ecfecab50d00c76c70ed56e593a7f
zmajcek <srdjan.bozovic@live.com>
2013-11-21 07:54:24 +0100

    app 2 app communication demo

---
+++ wp8/KursnaListaSolution/AppToAppCommunicationTestApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AppToAppCommunicationTestApp.Resources;
+
+namespace AppToAppCommunicationTestApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
