da655176e3c4199615a2eb2e0f1db289aadbf1ba
srdjanBozovic <srdjan.bozovic@live.com>
2014-09-01 01:01:23 +0200

    fix: git issues

---
+++ x/KursnaLista.Mobile/KursnaLista.Mobile/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace KursnaLista.Mobile
+{
+	public class App
+	{
+		private static ViewModelLocator _locator;
+
+		public static ViewModelLocator Locator
+		{
+			get
+			{
+				return _locator ?? (_locator = new ViewModelLocator());
+			}
+		}
+
+		public static Page GetMainPage ()
+		{	
+			return new MainPageView();
+		}
+	}
+}
+
