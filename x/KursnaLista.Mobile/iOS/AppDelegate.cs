a0df99eedd6509ba89c97a1464e640a6f951f6f3
nemanjastancic <unknown-user@192.168.0.52>
2014-09-04 20:38:08 +0200

    Add: - IOS project

---
+++ x/KursnaLista.Mobile/iOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+using MSC.Phone.Shared.Contracts.Services;
+using MSC.Phone.Shared;
+
+namespace KursnaLista.Mobile.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			Forms.Init ();
+
+			ViewModelLocator.InstanceFactory.RegisterType<ICacheService, NullCacheService>();
+
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+			window.RootViewController = App.GetMainPage ().CreateViewController ();
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
+
